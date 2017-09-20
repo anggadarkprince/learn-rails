c56d90ab7b66652e4a7d95b7fda54e788b339699
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-09-20 22:24:09 +0700

    add comment form and author in show article

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,10 +1,10 @@
 class CommentsController < ApplicationController
-  http_basic_authenticate_with name: "angga", password: "secret", only: :destroy
+  http_basic_authenticate_with name: 'angga', password: 'secret', only: :destroy
 
   def create
     @article = Article.find(params[:article_id])
     @comment = @article.comments.create(comment_params)
-    redirect_to article_path(@article)
+    redirect_to blog_show_path(slug: @article.slug), notice: 'Your comment was successfully submitted.'
   end
 
   def destroy
@@ -16,6 +16,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:commenter, :body)
+    params.require(:comment).permit(:name, :email, :comment)
   end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ApplicationRecord
   belongs_to :article
   has_many :comments, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 5 }
+  validates :email, presence: true
+  validates :comment, presence: true
 end
