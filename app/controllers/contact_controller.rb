4645218735c082f8f0745c9607a6201381556cce
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-09-26 20:33:34 +0700

    change save contact to remote

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -3,16 +3,35 @@ class ContactController < ApplicationController
   def form
   end
 
+  # POST /contact
+  # POST /contact.json
   def create
     @contact = Contact.new(params.require(:contact).permit(:name, :email, :subject, :message))
 
-    if @contact.save
-      # Tell the UserMailer to send a welcome email after save
-      ContactMailer.contact_email(@contact).deliver_later
+    respond_to do |format|
+      if @contact.save
+        # Tell the ContactMailer to send a welcome email after save
+        ContactMailer.contact_email(@contact).deliver_later
 
-      redirect_to('/'+I18n.default_locale.to_s+'/contact', notice: 'Message was successfully submitted.')
-    else
-      render action: 'form'
+        format.html {
+          redirect_to('/'+I18n.default_locale.to_s+'/contact', alert: 'success', notice: 'Message was successfully submitted.')
+        }
+        format.js {render 'form', locals: {alert: 'success', notice: 'Message was successfully submitted.'}} #default behaviour is to run app/views/contact/create.js.erb file
+        format.json {
+          render json: @contact, status: :created, location: @contact
+        }
+      else
+        error = @contact.errors.any? ? @contact.errors.full_messages[0] : 'Message failed to submit.'
+        format.html {
+          flash.now[:alert] = 'danger'
+          flash.now[:notice] = error
+          render action: 'form'
+        }
+        format.js {render 'form', locals: {alert: 'danger', notice: error}}
+        format.json {
+          render json: @contact.errors, status: :unprocessable_entity
+        }
+      end
     end
   end
 
--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ApplicationRecord
   validates :name, presence: true, length: { minimum: 5 }
+  validates :email, presence: true, format: {with: /\A[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\z/i}
+  validates :subject, presence: true
+  validates :message, presence: true
 end
