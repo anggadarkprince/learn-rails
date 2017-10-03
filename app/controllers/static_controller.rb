00080138e57144f70cbc31d7cc6c59ca70d4c767
Angga Ari Wijaya <anggadarkprince@gmail.com>
2017-10-03 20:33:21 +0700

    implement caching

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,15 @@
 class StaticController < ApplicationController
 
   def agreement
+    http_cache_forever(public: true) do
+      render
+    end
   end
 
   def privacy
+    http_cache_forever(public: true) do
+      render
+    end
   end
 
 end
